0f59f413a65b325fdb984f3d153d3ee0a8c4b5e7
herreriasjose <herreriasjose@gmail.com>
2019-02-13 14:14:07 +0100

    Begin to break the HTML request into pieces

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
+const path = require('path');
 
 const waf = require('./lib/wafnode');
 const logger = require('./lib/logger');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'static')));
 
 const port = 8080;
 
+
 app.use(waf({ option1: '1', option2: '2' }));
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile('index.html');
 });
 
 app.listen(port);
--- menu/values.js
+++
@@ -1,8 +0,0 @@
-/** This file provides a list of the available items in the menus */
-
-// analysis types
-exports.types = [
-  { name: 'SQL Injection', description: 'Attack on web database' },
-  { name: 'XSS Attack', description: 'Execute malicious script in victim\'s browser' },
-];
-exports.typesPlain = exports.types.map(o => `${o.name} (${o.description})`); // convert to one line
