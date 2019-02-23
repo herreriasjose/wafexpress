a800d40f033799ef78d8f7008c4eb65a64015d18
herreriasjose <herreriasjose@gmail.com>
2019-02-23 17:40:23 +0100

    Change structure

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ app.use(express.static(path.join(__dirname, 'static')));
 const port = 8080;
 
 
-app.use(waf({ option1: '1', option2: '2' }));
+app.use(waf({ modules: ["ScannerSQLInjection","ScannerXSSAttack"], option2: '2' }));
 app.get('/', (req, res) => {
   res.sendFile('index.html');
+});
+app.post('/api/scan', (req, res) => {
+  res.send("Reached endpoint")
 });
 
 app.listen(port);
