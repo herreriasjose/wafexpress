642b4e9fba3bf400e7e4310c9a9a34aebae58c2f
herreriasjose <herreriasjose@gmail.com>
2019-03-18 18:45:03 +0100

    Add HTTP server

---
+++ serverHttp.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const logger = require('./lib/logger');
+
+const server = http.createServer();
+
+const port = 8080;
+
+let handler = function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+};
+
+
+server.addListener('request', handler);
+server.listen(port);
+
+logger.info(`Http module server listening on port ${port}`);
